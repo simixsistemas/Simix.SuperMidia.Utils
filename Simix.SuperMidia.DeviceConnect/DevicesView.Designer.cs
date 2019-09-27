1361f633439ebbadfd8db03e398ce5624a9798aa
Felipe da silva baltazar <felipe.baltazar@simix.com.br>
2019-09-27 09:32:15 -0300

    Code review

---
+++ Simix.SuperMidia.DeviceConnect/DevicesView.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace RemoteDevices {
+    partial class DevicesView {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DevicesView));
+            this.grdDevices = new System.Windows.Forms.DataGridView();
+            this.deviceName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ipDevice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.connect = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.btnConnectAll = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.closeAction = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnMinimize = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.grdDevices)).BeginInit();
+            this.closeAction.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grdDevices
+            // 
+            this.grdDevices.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grdDevices.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.grdDevices.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SunkenHorizontal;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.ControlDarkDark;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.grdDevices.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.grdDevices.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdDevices.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.deviceName,
+            this.ipDevice,
+            this.status,
+            this.connect});
+            this.grdDevices.Location = new System.Drawing.Point(12, 27);
+            this.grdDevices.MultiSelect = false;
+            this.grdDevices.Name = "grdDevices";
+            this.grdDevices.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.AppWorkspace;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.grdDevices.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            this.grdDevices.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.grdDevices.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.grdDevices.Size = new System.Drawing.Size(594, 160);
+            this.grdDevices.TabIndex = 0;
+            this.grdDevices.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick);
+            this.grdDevices.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellEndEdit);
+            // 
+            // deviceName
+            // 
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.Gray;
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.deviceName.DefaultCellStyle = dataGridViewCellStyle2;
+            this.deviceName.HeaderText = "Device Name";
+            this.deviceName.Name = "deviceName";
+            this.deviceName.Width = 200;
+            // 
+            // ipDevice
+            // 
+            this.ipDevice.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.Gray;
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.Silver;
+            this.ipDevice.DefaultCellStyle = dataGridViewCellStyle3;
+            this.ipDevice.HeaderText = "Device IP";
+            this.ipDevice.Name = "ipDevice";
+            // 
+            // status
+            // 
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.Gray;
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.Silver;
+            this.status.DefaultCellStyle = dataGridViewCellStyle4;
+            this.status.HeaderText = "Status";
+            this.status.Name = "status";
+            this.status.ReadOnly = true;
+            // 
+            // connect
+            // 
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.Gray;
+            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.Color.Silver;
+            this.connect.DefaultCellStyle = dataGridViewCellStyle5;
+            this.connect.HeaderText = "Connect";
+            this.connect.Name = "connect";
+            // 
+            // btnConnectAll
+            // 
+            this.btnConnectAll.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnConnectAll.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnConnectAll.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConnectAll.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnConnectAll.Location = new System.Drawing.Point(12, 193);
+            this.btnConnectAll.Name = "btnConnectAll";
+            this.btnConnectAll.Size = new System.Drawing.Size(75, 23);
+            this.btnConnectAll.TabIndex = 1;
+            this.btnConnectAll.Text = "Connect all";
+            this.btnConnectAll.UseVisualStyleBackColor = false;
+            this.btnConnectAll.Click += new System.EventHandler(this.BtnConnectAll_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipText = "Click to show";
+            this.notifyIcon1.BalloonTipTitle = "Remote Devices";
+            this.notifyIcon1.ContextMenuStrip = this.closeAction;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "RemoteDevices";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon_MouseDoubleClick);
+            // 
+            // closeAction
+            // 
+            this.closeAction.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.closeToolStripMenuItem});
+            this.closeAction.Name = "closeAction";
+            this.closeAction.Size = new System.Drawing.Size(104, 26);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.CloseToolStripMenuItem_Click);
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMinimize.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnMinimize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinimize.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinimize.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnMinimize.Location = new System.Drawing.Point(579, 1);
+            this.btnMinimize.Margin = new System.Windows.Forms.Padding(0);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(27, 20);
+            this.btnMinimize.TabIndex = 3;
+            this.btnMinimize.Text = "-";
+            this.btnMinimize.UseVisualStyleBackColor = false;
+            this.btnMinimize.Click += new System.EventHandler(this.Hide_Click);
+            // 
+            // frmDevices
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(618, 228);
+            this.Controls.Add(this.btnMinimize);
+            this.Controls.Add(this.btnConnectAll);
+            this.Controls.Add(this.grdDevices);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmDevices";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Remote Devices";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grdDevices)).EndInit();
+            this.closeAction.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grdDevices;
+        private System.Windows.Forms.Button btnConnectAll;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip closeAction;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.Button btnMinimize;
+        private System.Windows.Forms.DataGridViewTextBoxColumn deviceName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ipDevice;
+        private System.Windows.Forms.DataGridViewTextBoxColumn status;
+        private System.Windows.Forms.DataGridViewButtonColumn connect;
+    }
+}
+
---
+++ Simix.SuperMidia.Utils.Core/Adb/LanguageMode.cs
@@ -0,0 +1,7 @@
+﻿namespace Simix.SuperMidia.Utils.Core.Adb {
+    public enum LanguageMode {
+        Pt_BR = 0,
+        Es_ES = 1,
+        En_US = 2
+    }
+}
